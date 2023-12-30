89ccc8cf69c489d3c158d6726d313cf046f4c8c4
Gilles Bertrand Tchako Ngangue <ngangueg@gmail.com>
2023-12-29 23:27:29 -0500

    Ajout bd

--- merchex/folio/admin.py
+++ merchex/folio/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from folio.models import UserProfile
+from folio.models import Skill
+from folio.models import Item
+from folio.models import Experience
+from folio.models import Task
+from folio.models import Formation
+
+class UserAdminAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'adresse', 'city') # liste les champs que nous voulons sur l'affichage de la liste
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'item') 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name') 
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('institution', 'type', 'title', 'city', 'country','started_at', 'ended_at', 'task') 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('description', 'technology') 
+class FormationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'institution', 'type', 'city', 'country','started_at', 'ended_at', 'in_progress') 
+
+admin.site.register(UserProfile, Skill, Item, Experience, Task, Formation, UserAdminAdmin, SkillAdmin,
+                    ItemAdmin, ExperienceAdmin, TaskAdmin, FormationAdmin)
+
--- merchex/folio/models.py
+++ merchex/folio/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.fields.CharField(max_length=200)
+    title = models.fields.CharField(max_length=200)
+    about = models.fields.CharField(max_length=200)
+    telephone = models.fields.CharField(max_length=200)
+    city = models.fields.CharField(max_length=200)
+    country = models.fields.CharField(max_length=200)
+    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    adresse = models.CharField(max_length=255, blank=True, null=True)
+    cv = models.FileField(upload_to='cv/', blank=True, null=True)
+
+    
+class Item(models.Model):
+    name = models.fields.CharField(max_length=25)
+
+class Formation(models.Model):
+    class Type(models.TextChoices):
+        Formation = 'Formation'
+        Certification = 'Certification'
+
+    type = models.fields.CharField(choices=type.choices, max_length=25)
+
+    name = models.fields.CharField(max_length=200)
+    institution = models.fields.CharField(max_length=50)
+    city = models.fields.CharField(max_length=50)
+    country = models.fields.CharField(max_length=50)
+    started_at = models.fields.DateField()
+    ended_at = models.fields.DateField()
+    in_progress = models.fields.BooleanField(default=False)
+
+class Task(models.Model):
+
+    description = models.fields.CharField(max_length=200)
+    technology = models.fields.CharField(max_length=55, blank=True, null=True)
+
+class Skill(models.Model):
+    name = models.fields.CharField(max_length=200)
+    photo = models.ImageField(upload_to='profile_photo/', blank=True, null=True)
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL)
+
+
+class Experience(models.Model):
+
+    class Type(models.TextChoices):
+        Emploi = 'Emploi'
+        Stage = 'Stage'
+        Projet = 'Projet'
+        Implication = 'Implication'
+
+    type = models.fields.CharField(choices=type.choices, max_length=25)
+
+    title = models.fields.CharField(max_length=200)
+    institution = models.fields.CharField(max_length=50)
+    city = models.fields.CharField(max_length=50)
+    country = models.fields.CharField(max_length=50)
+    started_at = models.fields.DateField()
+    ended_at = models.fields.DateField()
+    task = models.ForeignKey(Task, null=True, on_delete=models.SET_NULL)
+ 
